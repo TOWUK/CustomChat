4e15bd8386df1a19d3c44a6a8feb430dcd9c1a29
TOWUK <towuk@internet.ru>
2026-06-12 18:28:10 +0300

    Update CustomChat.java

--- src/CustomChat.java
+++ src/CustomChat.java
@@ -8,17 +8,13 @@ public class CustomChat extends Plugin {
     private String fmt(Player p, String prefix, String msg) { return prefix + p.name + "[#dadada]: " + msg; }
     @Override
     public void registerClientCommands(CommandHandler handler) {
-        handler.register("t", "<msg...>", "Team chat", (String[] a, Player p) ->
-            Groups.player.each(r -> r.team() == p.team(), r -> r.sendMessage(fmt(p, "[#dadada][T] ", a[0])))
-        );
-        handler.register("a", "<msg...>", "Admin chat", (String[] a, Player p) ->
-            Groups.player.each(Player::admin, r -> r.sendMessage(fmt(p, "[#dadada][A] ", a[0])))
-        );
+        handler.register("t", "<msg...>", "Team chat", (String[] a, Player p) -> Groups.player.each(r -> r.team() == p.team(), r -> r.sendMessage(fmt(p, "[#dadada][T] ", a[0]))) );
+        handler.register("a", "<msg...>", "Admin chat", (String[] a, Player p) -> Groups.player.each(Player::admin, r -> r.sendMessage(fmt(p, "[#dadada][A] ", a[0]))) );
     }
     @Override
     public void init() {
         Vars.netServer.admins.addChatFilter((p, m) -> {
-            if (p == null || m.startsWith("/")) return m;
+            if (m.startsWith("/")) return m;
             Call.sendMessage(fmt(p, "", m));
             return null;
         });
