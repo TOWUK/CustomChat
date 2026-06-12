ddff9e18cca2fe9ba31b7bc8a45f0ebe45a220a2
TOWUK <towuk@internet.ru>
2026-06-12 18:23:09 +0300

    Update CustomChat.java

--- src/CustomChat.java
+++ src/CustomChat.java
@@ -1,16 +1,27 @@
+import arc.util.CommandHandler;
 import mindustry.Vars;
 import mindustry.gen.Call;
+import mindustry.gen.Groups;
 import mindustry.gen.Player;
 import mindustry.mod.Plugin;
-
 public class CustomChat extends Plugin {
-
+    private String fmt(Player p, String prefix, String msg) {
+        return prefix + p.name + "[#dadada]: " + msg;
+    }
+    @Override
+    public void registerClientCommands(CommandHandler handler) {
+        handler.register("t", "<msg...>", "Team chat", (String[] a, Player p) ->
+            Groups.player.each(r -> r.team() == p.team(), r -> r.sendMessage(fmt(p, "[#dadada][T] ", a[0])))
+        );
+        handler.register("a", "<msg...>", "Admin chat", (String[] a, Player p) ->
+            Groups.player.each(Player::admin, r -> r.sendMessage(fmt(p, "[#dadada][A] ", a[0])))
+        );
+    }
     @Override
     public void init() {
-        Vars.netServer.admins.addChatFilter((p, msg) -> {
-            if (p == null) return null;
-            if (!msg.isEmpty() && msg.charAt(0) == '/') return msg;
-            Call.sendMessage(p.name + "[#dadada]: " + msg);
+        Vars.netServer.admins.addChatFilter((p, m) -> {
+            if (p == null || m.startsWith("/")) return m;
+            Call.sendMessage(fmt(p, "", m));
             return null;
         });
     }
